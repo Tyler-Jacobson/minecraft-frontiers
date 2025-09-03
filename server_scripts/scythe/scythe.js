6e117709133cf6650edfff5b7195f40e21737913
Tyler Jacobson <tylerjacobsonse@gmail.com>
2025-09-02 22:03:56 -0400

    scythe on click functionality

--- server_scripts/scythe/scythe.js
+++ server_scripts/scythe/scythe.js
@@ -1,24 +1,9 @@
-ItemEvents.firstLeftClicked("frontiers:scythe", event => {
-    console.info('left clicked')
+ItemEvents.firstLeftClicked("frontiers:custom_scythe", event => {
+    console.info('first left clicked')
 })
 
-// ForgeEvents.onEvent('item.left_click', e => {
-//   console.log('item left click')
-// })
-
-// ServerEvents.tick(event => {
-//     let player = event.getPlayer()
-//     player.stopAnimation("bettercombat:two_handed_slash_horizontal_right")
-//     player.stopAnimation("bettercombat:two_handed_slash_horizontal_left")
-//     player.stopAnimation("bettercombat:scythe_slash")
-//     player.stopAnimation("bettercombat:pose_two_handed_scythe")
-
-// })
-
-// PlayerEvents.tick((event) => {
-//     let player = event.getPlayer()
-//     player.stopAnimation("bettercombat:two_handed_slash_horizontal_right")
-//     player.stopAnimation("bettercombat:two_handed_slash_horizontal_left")
-//     player.stopAnimation("bettercombat:scythe_slash")
-//     player.stopAnimation("bettercombat:pose_two_handed_scythe")
-// })
+NetworkEvents.dataReceived('better_combat_scythe_attack_started', (event) => { // this runs on server side once when left clicking with the scythe
+    // this is all I needed. This code will run on scythe left click
+    console.info(`event data received ${event.data}`)
+    console.info(`event entity ${event.entity}`)
+});
