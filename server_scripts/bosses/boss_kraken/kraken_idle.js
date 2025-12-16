4b69738aa2761e0cb5551cd5b6302b588ec87a26
Tyler Jacobson <tylerjacobsonse@gmail.com>
2025-12-16 14:55:38 -0500

    initial movement script

--- server_scripts/bosses/boss_kraken/kraken_idle.js
+++ server_scripts/bosses/boss_kraken/kraken_idle.js
@@ -1,3 +1,5 @@
+const KRAKEN_MOVESPEED = 5
+
 const runIdle = (entity) => {
     let actionDuration = 100 // how long will the action take (in ticks)
     entity.persistentData.startNextActionAge = entity.age + actionDuration // set persistent data to run new action after this one finishes
@@ -5,4 +7,23 @@ const runIdle = (entity) => {
     entity.triggerAnimation('krakenBossController', 'k_idle') // play animations
     entity.persistentData.actionQueue = [] // clear the action queue
     // movement function here
+    moveToLocation(entity)
+}
+
+const moveToLocation = (entity) => {
+    let targetPlayer = getPriorityTarget(entity)
+    let currentMovement = entity.getDeltaMovement()
+
+    let distanceFromTargetPlayer = 20
+    let yOffsetFromTargetPlayer = 5
+    let randomMovementAngle = getRandomIntInclusive(0, 360)
+
+    let targetDestination = mobRelativeLocation(targetPlayer, distanceFromTargetPlayer, randomMovementAngle, yOffsetFromTargetPlayer)
+    let destinationAngle = angleVecFromAToB(entity.getEyePosition(), targetDestination)
+    const vel = destinationAngle.scale(KRAKEN_MOVESPEED)
+    entity.setMotion(vel.x(), vel.y(), vel.z())
+    // entity.setDeltaMovement(targetPlayer.getEyePosition())
+    // entity.setMotion(currentMovement.x(), currentMovement.y(), currentMovement.z())
+    // entity.setDeltaMovement(currentMovement)
+
 }
