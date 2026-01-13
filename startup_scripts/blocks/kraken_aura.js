8c5e89bbdfd01294624fb23a60b7563eb937e294
Tyler Jacobson <tylerjacobsonse@gmail.com>
2026-01-13 18:01:56 -0500

    add kraken aura laser entity

--- startup_scripts/blocks/kraken_aura.js
+++ startup_scripts/blocks/kraken_aura.js
@@ -1,6 +1,7 @@
-const KRAKEN_AURA_RADIUS = 2
+const KRAKEN_AURA_DISPLAY_RADIUS = 2
 const KRAKEN_AURA_Y_OFFSET = 1
 const KRAKEN_AURA_TICK_DELAY = true
+const KRAKEN_AURA_ATTACK_RADIUS = 20
 
 StartupEvents.registry("block", event => {
   event.create("frontiers:aura_projector").displayName("Aura Projector")
@@ -12,7 +13,9 @@ function findEntitiesWithinBlocks(level, coordinateX, coordinateY, coordinateZ, 
 
 StartupEvents.registry("block", event => {
   event.create("frontiers:kraken_aura")
-    .displayName("Kraken Aura Block")
+    .displayName("Kraken Aura Block")    
+    .hardness(2.0)
+    .tagBlock("minecraft:mineable/pickaxe")
     .blockEntity(entityInfo => { // also has tick and serverTick methods
       entityInfo.tick(1, 0, entity => {
         let level = entity.level
@@ -23,8 +26,8 @@ StartupEvents.registry("block", event => {
         if (level.time % 10 === 0) {
           for (let step = 0; step < 12; step++) {
             let angle = step * JavaMath.PI * 2 / 12
-            let particleX = posX + 0.5 + Math.cos(angle) * KRAKEN_AURA_RADIUS
-            let particleZ = posZ + 0.5 + Math.sin(angle) * KRAKEN_AURA_RADIUS
+            let particleX = posX + 0.5 + Math.cos(angle) * KRAKEN_AURA_DISPLAY_RADIUS
+            let particleZ = posZ + 0.5 + Math.sin(angle) * KRAKEN_AURA_DISPLAY_RADIUS
             // console.log(`spawning particles at ${particleX} ${pos[1] + 0.2} ${particleZ}`)
             if (KRAKEN_AURA_TICK_DELAY) {
               // console.log(`${Object.keys(level)}`)
@@ -39,7 +42,7 @@ StartupEvents.registry("block", event => {
           }
         }
         if (level.time % 100 === 0) {
-          let entitiesNearBlock = findEntitiesWithinBlocks(level, posX, posY, posZ, 20)
+          let entitiesNearBlock = findEntitiesWithinBlocks(level, posX, posY, posZ, KRAKEN_AURA_ATTACK_RADIUS)
           if (!(level === 'ClientLevel')) {
             if (!entitiesNearBlock.length) return
             let filteredEntities = entitiesNearBlock.filter(entity => {
@@ -70,7 +73,7 @@ StartupEvents.registry("block", event => {
 
 const spawnKrakenAuraProjectile = (mob, level, attackStartingLocation, lookAngle) => {
     // const { level } = mob
-    const projectile = level.createEntity("frontiers:kraken_red_projectile");
+    const projectile = level.createEntity("frontiers:kraken_aura_laser");
     // it's crucial to set the projectile entity's owner here, since we're later going to reference this in order to get the damage source
     projectile.setOwner(mob)
     const vel = lookAngle.scale(0.5)
--- startup_scripts/boss_kraken/kraken_entities.js
+++ startup_scripts/boss_kraken/kraken_entities.js
@@ -1,4 +1,5 @@
 let YELLOW_LASER_MOVE_SPEED = 0.3
+let AURA_LASER_MOVE_SPEED = 0
 
 
 
@@ -25,13 +26,13 @@ StartupEvents.registry('entity_type', event => {
 
             if (hitEntity.isPlayer() && hitEntity.isBlocking()) {
 
-                hitEntity.attack(damageSource, 5)
+                hitEntity.attack(damageSource, 23)
 
                 let lookAngle = hitEntity.lookAngle
                 const vel = lookAngle.scale(1.5)
                 entity.setMotion(vel.x(), vel.y(), vel.z())
             } else {
-                hitEntity.attack(damageSource, 5)
+                hitEntity.attack(damageSource, 23)
                 entity.kill()
             }
 
@@ -79,6 +80,8 @@ StartupEvents.registry('entity_type', event => {
             // // 'entity' in this context is the projectile that is spawned
             // // 'result.entity' in this context is the target that is hit by the projectile
             const { entity, result } = context;
+            if (result.entity.type === 'frontiers:custom_kraken' || result.entity.type === 'block_factorys_bosses:soul_skeleton') return
+
 
             // // The 'entity' (projectile) has a list of possible damage sources on it, accessed through damageSources()
             // // to set the player as the source of damage, we choose .playerAttack() as the damage source,
@@ -216,7 +219,7 @@ StartupEvents.registry('entity_type', event => {
             // // 'entity' in this context is the projectile that is spawned
             // // 'result.entity' in this context is the target that is hit by the projectile
             const { entity, result } = context;
-            if (result.entity.type === 'frontiers:custom_kraken') return
+            if (result.entity.type === 'frontiers:custom_kraken' || result.entity.type === 'block_factorys_bosses:soul_skeleton') return
 
             // // The 'entity' (projectile) has a list of possible damage sources on it, accessed through damageSources()
             // // to set the player as the source of damage, we choose .playerAttack() as the damage source,
@@ -355,5 +358,68 @@ StartupEvents.registry('entity_type', event => {
 
             world.spawnParticles("minecraft:smoke", false, collisionX, collisionY + smokeParticleYOffset, collisionZ, 0, 0, 0, smokeParticleCountPerTick, smokeParticleSpeedPerTick)
             world.spawnParticles("minecraft:lava", false, collisionX, collisionY, collisionZ, 0, 0, 0, lavaParticleCountPerTick, lavaParticleSpeedPerTick)
+        }).noItem()
+})
+
+StartupEvents.registry('entity_type', event => {
+    // frontiers:fireball_entity here references geo/entity/fireball_entity.geo.json and textures/entity/fireball_entity.png
+    const builder = event.create("frontiers:kraken_aura_laser", "entityjs:geckolib_projectile")
+        .newGlowingGeoLayer(builder => {
+            builder.textureResource(entity => {
+                // return some glowing texture overlay
+                return "frontiers:textures/entity/kraken_aura_laser.png"
+            })
+        })
+        .isAttackable(false)
+        .onHitEntity(context => {
+            // 'entity' in this context is the projectile that is spawned
+            // 'result.entity' in this context is the target that is hit by the projectile
+            const { entity, result } = context;
+            if (result.entity.type === 'frontiers:custom_kraken' || result.entity.type === 'block_factorys_bosses:soul_skeleton') return
+
+
+            const kraken = entity.getOwner()
+            if (!kraken) {
+                entity.kill()
+            }
+            const damageSource = entity.damageSources().mobProjectile(entity, kraken) // this is where we change the damage type
+            const hitEntity = result.entity
+            hitEntity.attack(damageSource, 16)
+
+            // we now get rid of the projectile entity
+            entity.kill()
+        }).onHitBlock(context => {
+            // const { entity } = context
+            // entity.kill()
+        }).tick(entity => {
+            const world = entity.level
+            const target = world.getNearestPlayer(entity, 64)
+
+            let destinationWithYOffset = new Vec3d(target.getEyePosition().x(), target.getEyePosition().y() - 0.5, target.getEyePosition().z())
+
+            let destinationAngle = global.angleVecFromAToB(entity.getEyePosition(), destinationWithYOffset)
+            const vel = destinationAngle.scale(AURA_LASER_MOVE_SPEED + (Math.log(entity.age) * 0.1))
+            entity.setMotion(vel.x(), vel.y(), vel.z())
+
+            const collisionX = entity.x
+            const collisionY = entity.y
+            const collisionZ = entity.z
+
+            if (entity.age === 3) {
+                world.playSound(entity, entity.block.pos, 'frontiers:fire_staff_fireball_projectile_whoosh', "players", 1, 1)
+            }
+
+            const smokeParticleYOffset = 0.3
+            const smokeParticleCountPerTick = 1
+            const smokeParticleSpeedPerTick = 0
+
+            const lavaParticleCountPerTick = 1
+            const lavaParticleSpeedPerTick = 20
+
+            world.spawnParticles("minecraft:smoke", false, collisionX, collisionY + smokeParticleYOffset, collisionZ, 0, 0, 0, smokeParticleCountPerTick, smokeParticleSpeedPerTick)
+            world.spawnParticles("minecraft:lava", false, collisionX, collisionY, collisionZ, 0, 0, 0, lavaParticleCountPerTick, lavaParticleSpeedPerTick)
+            if (entity.age > 300) {
+                entity.kill()
+            }
         }).noItem()
 })
