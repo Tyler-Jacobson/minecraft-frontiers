85e55afd21f7bd012aa14fca08b8113ff17e30ba
Tyler Jacobson <tylerjacobsonse@gmail.com>
2026-01-14 15:43:37 -0500

    vary dash distance for each unique kraken attack

--- server_scripts/bosses/boss_kraken/kraken_boss.js
+++ server_scripts/bosses/boss_kraken/kraken_boss.js
@@ -44,14 +44,14 @@ const startNewAction = (entity, event) => {
             break;
         case 'red':
             runRed(entity, event)
-            moveToLocation(entity, 20, 1) // moves the kraken to a location 20 blocks from the player
+            moveToLocation(entity, 40, 1, 3) // moves the kraken to a location 20 blocks from the player
             break;
         case 'yellow':
             runYellow(entity, event)
             break;
         case 'blue':
             runBlue(entity, event)
-            moveToLocation(entity, 5, 1) // moves the kraken to a location 5 blocks from the player
+            moveToLocation(entity, 5, 1, 0) // moves the kraken to a location 5 blocks from the player
             break;
         case 'white':
             runWhite(entity, event) // giga laser
