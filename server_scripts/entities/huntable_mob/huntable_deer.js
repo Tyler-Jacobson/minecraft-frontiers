aaf96e4514b254b476da0694ec376076ecb58363
Tyler Jacobson <tylerjacobsonse@gmail.com>
2026-01-29 12:59:04 -0500

    restructure huntable mob files

--- server_scripts/entities/huntable_mob/huntable_deer.js
+++ server_scripts/entities/huntable_mob/huntable_deer.js
@@ -1,9 +1,7 @@
 let LivingEntity = Java.loadClass('net.minecraft.world.entity.LivingEntity')
 let CustomGoal = Java.loadClass("net.liopyu.entityjs.util.ai.CustomGoal")
 let $MoveGoalFlag = Java.loadClass("net.minecraft.world.entity.ai.goal.Goal$Flag")
-let BlockPos = Java.loadClass("net.minecraft.core.BlockPos");
 let DefaultRandomPos = Java.loadClass("net.minecraft.world.entity.ai.util.DefaultRandomPos")
-let BlockPathTypes = Java.loadClass("net.minecraft.world.level.pathfinder.BlockPathTypes")
 
 
 EntityJSEvents.addGoalSelectors('frontiers:huntable_deer_test', event => { // goal selectors
@@ -119,52 +117,3 @@ EntityJSEvents.addGoals('frontiers:huntable_deer_test', event => { // target sel
     // console.log(`target selector ${Object.keys(mob.targetSelector)}`)
     // event.nearestAttackableTarget(2, LivingEntity, 10, true, false, t => global.canAttackNearbyTarget(mob, t), mob.boundingBox.inflate(followRange, 25, followRange))
 })
-
-global.runDampedSwim = entity => {
-    if (entity.isInWater()) {
-        let currentX = entity.getDeltaMovement().x()
-        let currentZ = entity.getDeltaMovement().z()
-        entity.setDeltaMovement(new Vec3d(currentX, 0.05, currentZ))
-    }
-}
-
-global.runNavigateToBait = entity => {
-    try {
-        if (!(entity.level === 'ClientLevel')) {
-            console.log(`entity.getSyncedData('timeSpentAtCurrentLocation') ${entity.getSyncedData('timeSpentAtCurrentLocation')}`)
-            let targetX = entity.getSyncedData('ownerBlockLocationX')
-            let targetY = entity.getSyncedData('ownerBlockLocationY')
-            let targetZ = entity.getSyncedData('ownerBlockLocationZ')
-            if (entity.isInWater()) {
-                try {
-                    // entity.setNavigation(EntityJSUtils.createWaterBoundPathNavigation(entity, entity.level))
-                    // entity.setPathfindingMalus(BlockPathTypes.WATER, 0.0)
-                    // entity.setPathfindingMalus(BlockPathTypes.WATER_BORDER, 0.0)
-                    // entity.setPathfindingMalus(BlockPathTypes.WALKABLE, 0.0)
-                    entity.getLookControl().setLookAt(targetX, targetY, targetZ)
-                    entity.getNavigation().moveTo(targetX, targetY, targetZ, 10)
-                    // entity.jump()
-                } catch (err) {
-                    console.log(`error setting pathfinding malice ${err}`)
-                }
-                // console.log('water nav mode')
-            } else {
-                // entity.setNavigation(EntityJSUtils.createGroundPathNavigation(entity, entity.level))
-                entity.getNavigation().moveTo(targetX, targetY, targetZ, 0.5)
-                console.log('land nav mode')
-            }
-        }
-
-
-    } catch (err) {
-        console.log(`failed to runNavigateToBait ${err}`)
-    }
-}
-
-global.runUnstuckPanic = entity => {
-    let randomPos = DefaultRandomPos.getPos(entity, 5, 10);
-    if (randomPos && entity.getNavigation().isDone()) {
-        entity.getNavigation().moveTo(randomPos.x(), randomPos.y(), randomPos.z(), 1)
-        console.log(`running unstuck panic navigation`)
-    }
-}
---
+++ server_scripts/entities/huntable_mob/huntable_mob_helpers.js
@@ -0,0 +1,52 @@
+let BlockPathTypes = Java.loadClass("net.minecraft.world.level.pathfinder.BlockPathTypes")
+
+
+
+global.runDampedSwim = entity => {
+    if (entity.isInWater()) {
+        let currentX = entity.getDeltaMovement().x()
+        let currentZ = entity.getDeltaMovement().z()
+        entity.setDeltaMovement(new Vec3d(currentX, 0.05, currentZ))
+    }
+}
+
+global.runNavigateToBait = entity => {
+    try {
+        if (!(entity.level === 'ClientLevel')) {
+            console.log(`entity.getSyncedData('timeSpentAtCurrentLocation') ${entity.getSyncedData('timeSpentAtCurrentLocation')}`)
+            let targetX = entity.getSyncedData('ownerBlockLocationX')
+            let targetY = entity.getSyncedData('ownerBlockLocationY')
+            let targetZ = entity.getSyncedData('ownerBlockLocationZ')
+            if (entity.isInWater()) {
+                try {
+                    // entity.setNavigation(EntityJSUtils.createWaterBoundPathNavigation(entity, entity.level))
+                    // entity.setPathfindingMalus(BlockPathTypes.WATER, 0.0)
+                    // entity.setPathfindingMalus(BlockPathTypes.WATER_BORDER, 0.0)
+                    // entity.setPathfindingMalus(BlockPathTypes.WALKABLE, 0.0)
+                    entity.getLookControl().setLookAt(targetX, targetY, targetZ)
+                    entity.getNavigation().moveTo(targetX, targetY, targetZ, 10)
+                    // entity.jump()
+                } catch (err) {
+                    console.log(`error setting pathfinding malice ${err}`)
+                }
+                // console.log('water nav mode')
+            } else {
+                // entity.setNavigation(EntityJSUtils.createGroundPathNavigation(entity, entity.level))
+                entity.getNavigation().moveTo(targetX, targetY, targetZ, 0.5)
+                console.log('land nav mode')
+            }
+        }
+
+
+    } catch (err) {
+        console.log(`failed to runNavigateToBait ${err}`)
+    }
+}
+
+global.runUnstuckPanic = entity => {
+    let randomPos = DefaultRandomPos.getPos(entity, 5, 10);
+    if (randomPos && entity.getNavigation().isDone()) {
+        entity.getNavigation().moveTo(randomPos.x(), randomPos.y(), randomPos.z(), 1)
+        console.log(`running unstuck panic navigation`)
+    }
+}
