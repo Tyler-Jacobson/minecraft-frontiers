72ad9507ab6a3e9091998d4f36292eee06666e1c
Tyler Jacobson <tylerjacobsonse@gmail.com>
2026-03-01 17:32:04 -0500

    fix carcass put on hook not playing hand animation

--- client_scripts/zombie_crow/zombie_crow_carcass_placement.js
+++ client_scripts/zombie_crow/zombie_crow_carcass_placement.js
@@ -1,6 +1,6 @@
 BlockEvents.rightClicked(event => {
     if (event.item.id !== 'frontiers:zombie_crow_carcass') return
-    if (event.block.id === 'butchersdelight:hook') {
+    if (event.block.id === 'frontiers:meat_hook') {
         event.player.swing(event.hand, true)
     }
     event.cancel()
--- startup_scripts/huntable_entities/zombie_crow/zombie_crow_carcass.js
+++ startup_scripts/huntable_entities/zombie_crow/zombie_crow_carcass.js
@@ -2,6 +2,5 @@ StartupEvents.registry('block', event => {
     event.create('frontiers:zombie_crow_carcass')
         .displayName('Zombie Crow Carcass')
         .notSolid()
-        // .noOcclusion() // this method does not exist
         .noCollision()
 })
