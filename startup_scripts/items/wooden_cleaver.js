5df0fe49d5d59ea5bc04db9099f50bab2f55f693
Tyler Jacobson <tylerjacobsonse@gmail.com>
2026-03-01 19:19:41 -0500

    add wooden cleaver item

---
+++ startup_scripts/items/wooden_cleaver.js
@@ -0,0 +1,7 @@
+StartupEvents.registry("item", event => {
+    event.create("frontiers:wooden_cleaver", 'sword')
+        .displayName('Wooden Cleaver')
+        .attackDamageBaseline(3)
+        .speedBaseline(-3.1)
+        .maxDamage(59)
+})
